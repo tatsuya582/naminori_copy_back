90213582a8c3782d2cefde4f9f7ba9c8851c2c8c
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-11 23:35:38 +0900

    add: ユーザー用のAPIルートを追加し、ログイン、ログアウト、サインアップのパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      devise_for :users,
+           controllers: {
+             registrations: "api/v1/users/registrations",
+             sessions: "api/v1/users/sessions"
+           },
+           path: "",
+           path_names: {
+             sign_in: "login",
+             sign_out: "logout",
+             registration: "signup"
+           },
+           defaults: { format: :json },
+           skip: [ :passwords, :confirmations, :unlocks ]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
