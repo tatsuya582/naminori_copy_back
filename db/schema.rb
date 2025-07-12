bb779269c923f88892ef05c1ac79e29447187757
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-12 21:50:00 +0900

    add: jwt_denylistsにインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_124409) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_124917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_12_124409) do
     t.datetime "exp", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti", unique: true
   end
 
   create_table "users", force: :cascade do |t|
