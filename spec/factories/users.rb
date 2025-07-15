45d44655216245af88e2e77d14b92204bd7d1860
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-15 15:35:02 +0900

    add: Userモデルのテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :user do
+    first_name { "太郎" }
+    last_name  { "山田" }
+    first_name_kana { "たろう" }
+    last_name_kana  { "やまだ" }
+    birth_date { Date.new(2000, 1, 1) }
+    phone_number { "09012345678" }
+    gender { :male }
+    prefecture { "東京都" }
+
+    email { Faker::Internet.email }
+    password { "password123" }
+    password_confirmation { "password123" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    subject { build(:user) }
+
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:first_name_kana) }
+    it { is_expected.to validate_presence_of(:last_name_kana) }
+    it { is_expected.to validate_presence_of(:birth_date) }
+    it { is_expected.to validate_presence_of(:phone_number) }
+    it { is_expected.to validate_presence_of(:gender) }
+    it { is_expected.to validate_presence_of(:prefecture) }
+
+    it { is_expected.to define_enum_for(:gender).with_values(male: 1, female: 2, other: 3) }
+  end
 end
