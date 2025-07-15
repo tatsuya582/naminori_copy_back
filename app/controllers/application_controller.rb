294dfce1f5f497e2c14ee37d7973443c3fb9bfdb
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-15 16:06:05 +0900

    fix: サインアップとアカウント更新のパラメータを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,15 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :password_confirmation ])
+    added_keys = [
+      :email, :password, :password_confirmation,
+      :first_name, :last_name,
+      :first_name_kana, :last_name_kana,
+      :birth_date, :phone_number,
+      :gender, :prefecture
+    ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_keys)
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(:account_update, keys: added_keys)
   end
 end
