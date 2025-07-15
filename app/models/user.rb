9854841441f453a0b71aed0227c3b33dedf54e29
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-15 15:08:27 +0900

    add: バリデーションとenumを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  enum gender: { male: 1, female: 2, other: 3 }, _prefix: true
+
+  validates :first_name, :last_name, :birth_date, :first_name_kana, :last_name_kana, :phone_number, :gender, :prefecture, presence: true
 end
