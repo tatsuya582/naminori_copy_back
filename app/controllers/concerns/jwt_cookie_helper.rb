3e5f89cc217b281e60c2fba963969a5f698093ab
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-07-24 11:55:21 +0900

    add: ログアウトが本番環境でcookiesを削除できてなかったのを修正

--- app/controllers/concerns/jwt_cookie_helper.rb
+++ app/controllers/concerns/jwt_cookie_helper.rb
@@ -2,20 +2,27 @@ module JwtCookieHelper
   def set_jwt_cookie(resource)
     token = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil).first
 
-    same_site =
-      if Rails.env.production?
-        allowed_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "").split(",")
-        origin = request.headers["Origin"]
-        allowed_origins.include?(origin) ? :none : :lax
-      else
-        :lax
-      end
-
     cookies.signed[:jwt] = {
       value: token,
       httponly: true,
       secure: Rails.env.production?,
-      same_site: same_site
+      same_site: resolve_same_site_policy
     }
+  end
+
+  def delete_jwt_cookie
+    cookies.delete(:jwt, secure: Rails.env.production?, same_site: resolve_same_site_policy)
+  end
+
+  private
+
+  def resolve_same_site_policy
+    if Rails.env.production?
+      allowed_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "").split(",")
+      origin = request.headers["Origin"]
+      allowed_origins.include?(origin) ? :none : :lax
+    else
+      :lax
+    end
   end
 end
